ad389b05697f1c53d9756c07e45caba5376ddcac
Ahmet <ahmetbarut155@gmail.com>
2024-01-28 19:51:53 +0300

    Add project files.

---
+++ CONVERSION/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CONVERSION
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+
+        {
+
+            double sayi = 5.32;
+
+           int a = Convert.ToInt32(sayi);
+
+            Console.WriteLine(a);
+            Console.WriteLine(a.GetType());
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
