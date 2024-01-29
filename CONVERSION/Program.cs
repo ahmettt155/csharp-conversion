d112dccdddd1c93971b1177ac2a0489d3e0054f8
Ahmet <ahmetbarut155@gmail.com>
2024-01-29 21:00:42 +0300

    3 notun ortalamasını alıp S+,A+,B+,F- vermesi

--- CONVERSION/Program.cs
+++ CONVERSION/Program.cs
@@ -9,30 +9,44 @@ namespace CONVERSION
     internal class Program
     {
         static void Main(string[] args)
-            
+
 
 
         {
-            Console.WriteLine("notunuzu giriniz");
-            string not = Console.ReadLine();
-            int notInt = Convert.ToInt32(not);
+            Console.WriteLine("1. notunuzu girinz");
+            int not1 = Convert.ToInt32(Console.ReadLine());
 
-            if (notInt > 50)
-                Console.WriteLine("geçti");
+            Console.WriteLine("2. notunuzu giriniz");
+            int not2 = Convert.ToInt32(Console.ReadLine());
 
+            Console.WriteLine("3. notu giriniz");
+            int not3 = Convert.ToInt32(Console.ReadLine());
+
+            int ortalama = (not1 + not2 + not3) / 3;
+
+            Console.WriteLine("ortalamanız = " + ortalama);
+
+
+            if (ortalama >= 60)
+
+            {
+                if (ortalama >= 80)
+                    Console.WriteLine("S+");
+
+                else
+                    Console.WriteLine("A+");
+
+            }
             else
-                Console.WriteLine("kaldı");
+            {
+                if (ortalama >= 40)
+                    Console.WriteLine("B+");
 
+                else
+                    Console.WriteLine("F");
 
+            }
             Console.ReadLine();
-
-
-
-
-
-
-
-
 
         }
     }
