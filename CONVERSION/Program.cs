b0aa14a2f0ba3efcb119f2affafb3d43dd0b1e83
Ahmet <ahmetbarut155@gmail.com>
2024-03-05 22:13:58 +0300

    deneme

--- CONVERSION/Program.cs
+++ CONVERSION/Program.cs
@@ -71,7 +71,7 @@ namespace CONVERSION
             {
                 Console.WriteLine("Geçerli bir işlem giriniz");
 
-                goto baslangic; 
+                goto baslangic;  
             }
         }
         
